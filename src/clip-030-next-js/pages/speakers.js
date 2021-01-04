8f62c16b6ff5857d26d1d7cca37efbfb710da80e
dmytrostrembetskyi <dmytrostrembetskyi@gmail.com>
2021-01-04 20:42:54 +0200

    Use context

--- src/clip-030-next-js/pages/speakers.js
+++ src/clip-030-next-js/pages/speakers.js
@@ -1,19 +1,27 @@
-import Header from '../src/components/Header/Header';
-import Menu from '../src/components/Menu/Menu';
-import SpeakerSearchBar from '../src/components/SpeakerSearchBar/SpeakerSearchBar';
-import Speakers from '../src/components/Speakers/Speakers';
-import Footer from '../src/components/Footer/Footer';
+import Header from "../src/components/Header/Header";
+import Menu from "../src/components/Menu/Menu";
+import SpeakerSearchBar from "../src/components/SpeakerSearchBar/SpeakerSearchBar";
+import Speakers from "../src/components/Speakers/Speakers";
+import Footer from "../src/components/Footer/Footer";
+import SpeakerContext from "../src/components/Speakers/SpeakerContext";
 
 function Page() {
-    return (
-        <div>
-            <Header />
-            <Menu />
-            <SpeakerSearchBar />
-            <Speakers />
-            <Footer />
-        </div>
-    );
+  const speakers = [
+    { imageSrc: "speaker-component-1124", name: "Douglas Croskford" },
+    { imageSrc: "speaker-component-1530", name: "Tamara Baker" },
+    { imageSrc: "speaker-component-10803", name: "Eugene Chuvyrov" },
+  ];
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <SpeakerContext.Provider value={speakers}>
+        <SpeakerSearchBar />
+        <Speakers />
+      </SpeakerContext.Provider>
+      <Footer />
+    </div>
+  );
 }
 
 export default Page;
--- src/clip-030-next-js/src/components/Speakers/Speakers.js
+++ src/clip-030-next-js/src/components/Speakers/Speakers.js
@@ -1,29 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 // import withData from "./withData";
-import SpeakerRenderProps from "./SpeakersRenderProps";
-
+//import SpeakerRenderProps from "./SpeakersRenderProps";
+import SpeakerContext from './SpeakerContext';
 
 const Speakers = () => {
+  const speakers = useContext(SpeakerContext);
   return (
-    <SpeakerRenderProps>
-      {({ speakers }) => {
-        return (
-          <div>
-            {speakers.map(({ imageSrc, name }) => {
-              return (
-                <img
-                  src={`/images/${imageSrc}.png`}
-                  alt={name}
-                  key={imageSrc}
-                ></img>
-              );
-            })}
-          </div>
-        );
-      }}
-    </SpeakerRenderProps>
+    <div>
+      {speakers.map(({ imageSrc, name }) => (
+        <img src={`/images/${imageSrc}.png`} alt={name} key={imageSrc}></img>
+      ))}
+    </div>
   );
 };
+
+// const Speakers = () => {
+//   return (
+//     <SpeakerRenderProps>
+//       {({ speakers }) => {
+//         return (
+//           <div>
+//             {speakers.map(({ imageSrc, name }) => {
+//               return (
+//                 <img
+//                   src={`/images/${imageSrc}.png`}
+//                   alt={name}
+//                   key={imageSrc}
+//                 ></img>
+//               );
+//             })}
+//           </div>
+//         );
+//       }}
+//     </SpeakerRenderProps>
+//   );
+// };
 
 // const maxSpeakersToshow = 2;
 // export default withData(maxSpeakersToshow)(Speakers);
